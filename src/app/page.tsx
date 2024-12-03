b0e13fe54650e5af67b5cd8d52a0f8caceeff225
domambia <omambiadauglous@gmail.com>
2024-12-03 08:16:23 +0300

    create navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
 "use client";
+import { NavSection } from "@/components/navbar";
 import styled from "styled-components";
 
 export default function Home() {
-  return <HeaderWrapper>Private Round</HeaderWrapper>;
+  return (
+    <HeaderWrapper>
+      <NavSection />
+    </HeaderWrapper>
+  );
 }
 
 const HeaderWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 1rem;
+  color: white;
+  padding: 1rem 3rem;
 `;
+
+const HeadingSection = styled.div``;
+const HeaderCardSection = styled.div``;
+
+const PageHeader = styled.h1``;
+const headerSpanPait = styled.span``;
+const headerSpanText = styled.span``;
---
+++ src/components/navbar.tsx
@@ -0,0 +1,71 @@
+"use client";
+import { MoveUpRightIcon } from "lucide-react";
+import styled from "styled-components";
+
+export const Navbar = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const NavLogo = styled.div``;
+export const NavLogoImg = styled.img``;
+export const NavItems = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  gap: 1rem;
+  text-decoration: none;
+  list-style-type: none;
+`;
+export const NavItem = styled.li``;
+export const NavItemLink = styled.a`
+  color: white;
+  font-size: 14px;
+`;
+export const NavActions = styled.div``;
+export const ConnectWalletButton = styled.div`
+  height: 55px;
+  width: 196px;
+  background-color: #8cd2cf;
+  padding: 18px 31px;
+  display: flex;
+  justify-content: center;
+  border-radius: 4px;
+  align-items: center;
+  gap: 4px;
+  font-size: 14px;
+`;
+export const ConnectWalletButtonLabel = styled.span`
+  color: white;
+`;
+
+export const NavSection = () => {
+  return (
+    <Navbar>
+      <NavLogo>
+        <NavLogoImg src="/Logo.svg" />
+      </NavLogo>
+      <NavItems>
+        <NavItem>
+          <NavItemLink>Home</NavItemLink>
+        </NavItem>
+        <NavItem>
+          <NavItemLink>WhitePaper</NavItemLink>
+        </NavItem>
+        <NavItem>
+          <NavItemLink>Join Telegram</NavItemLink>
+        </NavItem>
+        <NavItem>
+          <NavItemLink>Contact</NavItemLink>
+        </NavItem>
+      </NavItems>
+      <NavActions>
+        <ConnectWalletButton>
+          <ConnectWalletButtonLabel>Connect Wallet</ConnectWalletButtonLabel>
+          <MoveUpRightIcon size={12} />
+        </ConnectWalletButton>
+      </NavActions>
+    </Navbar>
+  );
+};
